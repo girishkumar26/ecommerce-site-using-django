cce44fef5c1d681ab1466d2534c4d9be373b7646
girissn <girissn@amazon.com>
2020-07-28 01:08:49 +0530

    updated the template html files

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('product-single', views.product, name='product-single'),
+    path('cart.html', views.cart, name='cart'),
+    path('shop.html', views.shop, name='shop'),
+    path('checkout.html', views.checkout, name='checkout'),
+
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -8,3 +8,15 @@ def home(request):
 
 def product(request):
     return render(request, 'product-single.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+
+def shop(request):
+    return render(request, 'shop.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
